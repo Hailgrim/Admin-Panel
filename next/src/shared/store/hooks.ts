a832abff6c772b19992e6f964a59d8aa1326f287
Hailgrim <rasstrelyat@gmail.com>
2024-11-10 00:48:20 +0400

    Redux types update

--- next/src/shared/store/hooks.ts
+++ next/src/shared/store/hooks.ts
@@ -1,6 +1,6 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { AppDispatch, RootState } from './store';
+import type { RootState, AppDispatch } from './store';
 
-export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const useAppSelector = useSelector.withTypes<RootState>();
